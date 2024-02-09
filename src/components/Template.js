69b62add8fea772a9750dfaebae7123d07f6a71e
Amit Kumar <15000amitkumar@gmail.com>
2024-02-09 21:56:10 +0530

    Template of Resgistration Page

--- src/components/Template.js
+++ src/components/Template.js
@@ -1,7 +1,44 @@
 import React from "react";
+import frameImage from "../assets/frame.webp";
 
-function Template() {
-  return <div>Template</div>;
+function Template({ title, desc1, desc2, image, formType, setisLoggedIn }) {
+  return (
+    <div>
+      <div>
+        <h1>{title}</h1>
+        <p>
+          <span>{desc1}</span>
+          <span>{desc2}</span>
+        </p>
+        {formType === "signup" ? <SignupForm /> : <LoginForm />}
+
+        <div>
+          <div></div>
+          <p>OR</p>
+          <div></div>
+        </div>
+        <button>
+          <p>Sign in with Google</p>
+        </button>
+      </div>
+      <div>
+        <img
+          src={frameImage}
+          alt="Pattern"
+          width={558}
+          height={504}
+          loading="lazy"
+        />
+        <img
+          src={image}
+          alt="Students"
+          width={558}
+          height={490}
+          loading="lazy"
+        />
+      </div>
+    </div>
+  );
 }
 
 export default Template;
